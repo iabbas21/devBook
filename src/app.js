5e26bb0c924d9e110082641f482096f53a3d34e8
iabbas21 <i.irfanabbas1996@gmail.com>
2026-02-24 23:14:57 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ const express = require('express')
 
 const app = express()
 
-app.use('/hello', (req, res) => {
-    res.send('Hello World!')
+app.use('/user', (req, res) => {
+    res.send('ha ha ha')
 })
 
+app.get('/user', (req, res) => {
+    res.send({ firstName: 'John', lastName: 'Doe' })
+})
+
+app.post('/user', (req, res) => {
+    res.send('User created successfully!')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('User deleted successfully!')
+})
+
+// This will match all HTTP methods (GET, POST, PUT, DELETE, etc.) for the route '/test'
 app.use('/test', (req, res) => {
     res.send('Hello from the server...!')
-})
-
-app.use('/', (req, res) => {
-    res.send('Welcome to dashboard page!')
 })
 
 app.listen(3000, () => {
